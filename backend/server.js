b663f1eec5e7a89bde4099e6a203173bca9e9fd9
Breekorir <violetlamai521@gmail.com>
2025-07-10 10:32:56 +0300

    ne

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 const mysql = require('mysql2/promise');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const SECRET_KEY = 'poultrySecretKey';
 
 // MySQL connection pool
